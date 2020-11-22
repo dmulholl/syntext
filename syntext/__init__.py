6cfd5e604c557b9953a27c60e66aaeb44094ff18
Darren Mulholland <dmulholl@tcd.ie>
2020-11-22 15:30:46 +0000

    Bump version to 2.7.0

--- syntext/__init__.py
+++ syntext/__init__.py
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------
 
 # Package version number.
-__version__ = "2.6.2"
+__version__ = "2.7.0"
 
 from .interface import render
 from .interface import main
